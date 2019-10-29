22a4c2ed0449840bfa81125c8fe507cdc8d5ac2d
Full name <dionjatingaleh@gmail.com>
2019-10-29 09:30:45 +0700

    [FRONT END] Finished Setting Paging CSS, Create Paging Tab CSS, Create Setting Table, Create Setting Navigation ##16

---
+++ app/components/AddNewRoomButton/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * AddNewRoomButton
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function AddNewRoomButton() {
+  return <div />;
+}
+
+AddNewRoomButton.propTypes = {};
+
+export default AddNewRoomButton;
--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -20,58 +20,110 @@ import NavBar from 'components/NavBar';
 import TopStats from 'components/TopStats';
 import Chart from 'components/Content';
 import SubHeader from 'components/SubHeader';
+import EventSpaceSettingTable from 'components/EventSpaceSettingTable';
+import MembershipSettingTable from 'components/MembershipSettingTable';
+import PrivateOfficeSettingTable from 'components/PrivateOfficeSettingTable';
+import MeetingroomsSettingTable from 'components/MeetingroomsSettingTable';
+import SettingNavigationTab from 'components/SettingNavigationTab';
 
-const MainContent = props => {
-  switch (props.path) {
-    case '/point-of-sales':
-      return <div>{props.path}</div>;
-
-    case '/event':
-      return <div>{props.path}</div>;
-
-    case '/event-space':
-      return <div>{props.path}</div>;
-
-    case '/meeting-rooms':
-      return <div>{props.path}</div>;
-
-    case '/membership':
-      return <div>{props.path}</div>;
-
-    case '/door-lock':
-      return <div>{props.path}</div>;
-
-    case '/accounting':
-      return <div>{props.path}</div>;
-
-    case '/report':
-      return <div>{props.path}</div>;
-
-    case '/settings':
-      return <div>{props.path}</div>;
-
-    case '/private-office':
-      return <div>{props.path}</div>;
+const TableSettings = props => {
+  switch (props.params) {
+    case 'event-space':
+      return <EventSpaceSettingTable />;
+      break;
+    case 'private-office':
+      return <PrivateOfficeSettingTable />;
+      break;
+    case 'membership':
+      return <MembershipSettingTable />;
+      break;
 
     default:
-      return (
-        <>
-          <div>
-            <Chart path={props.path} />
-          </div>
-          <div>
-            <TopStats />
-          </div>
-        </>
-      );
+      return <MeetingroomsSettingTable />;
   }
+};
+
+const MainContent = props => {
+  if (!props.params) {
+    switch (props.path) {
+      case '/point-of-sales':
+        return <div>{props.path}</div>;
+
+      case '/event':
+        return <div>{props.path}</div>;
+
+      case '/event-space':
+        return <div>{props.path}</div>;
+
+      case '/meeting-rooms':
+        return <div>{props.path}</div>;
+
+      case '/membership':
+        return <div>{props.path}</div>;
+
+      case '/door-lock':
+        return <div>{props.path}</div>;
+
+      case '/accounting':
+        return <div>{props.path}</div>;
+
+      case '/report':
+        return <div>{props.path}</div>;
+
+      case '/settings':
+        return (
+          <div
+            style={{
+              width: '100%',
+              paddingRight: '30px',
+              paddingBottom: '30px',
+            }}
+          >
+            <SettingNavigationTab params={props.params} />
+            <TableSettings params={props.params} />
+          </div>
+        );
+
+      case '/private-office':
+        return <div>{props.path}</div>;
+
+      default:
+        return (
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 0.5fr',
+              gridGap: '30px',
+            }}
+          >
+            <div>
+              <Chart path={props.path} />
+            </div>
+            <div>
+              <TopStats />
+            </div>
+          </div>
+        );
+    }
+  }
+  return (
+    <div
+      style={{
+        width: '100%',
+        paddingRight: '30px',
+        paddingBottom: '30px',
+      }}
+    >
+      <SettingNavigationTab params={props.params} />
+      <TableSettings params={props.params} />
+    </div>
+  );
 };
 
 export function Dashboard(props) {
   useInjectReducer({ key: 'dashboard', reducer });
   useInjectSaga({ key: 'dashboard', saga });
 
-  console.log(props.location.pathname);
   return (
     <div>
       <div
@@ -82,7 +134,7 @@ export function Dashboard(props) {
         }}
       >
         <NavBar />
-        <SubHeader path={props.location.pathname} />
+        <SubHeader path={props.match.path} params={props.match.params} />
         <div
           style={{
             display: 'grid',
@@ -97,12 +149,12 @@ export function Dashboard(props) {
           <div
             style={{
               height: '100%',
-              display: 'grid',
-              gridTemplateColumns: '1fr 0.5fr',
-              gridGap: '30px',
             }}
           >
-            <MainContent path={props.location.pathname} />
+            <MainContent
+              path={props.match.path}
+              params={props.match.params.subpages}
+            />
           </div>
         </div>
       </div>
